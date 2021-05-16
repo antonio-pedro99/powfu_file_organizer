8e3c6124330e901c54131d8319fa691bc3d37096
Nurul-GC <nuruldecarvalho@gmail.com>
2021-05-16 02:07:28 +0200

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -1,80 +1,80 @@
-import os, sys, filecmp
 """
     This module contains all functions and variables needed to handle extensions.
 """
 
+import os
+
 image_exts = [
-    ".jpg", 
+    ".jpg",
     ".jpeg",
     ".png",
-    ".heic", 
+    ".heic",
     ".webp",
-    ".jpe", 
-    ".jfif", 
-    ".tif", 
-    ".tiff", 
+    ".jpe",
+    ".jfif",
+    ".tif",
+    ".tiff",
     ".gif",
     ".bmp",
     ".dib",
     ".ico"]
 
-
 music_exts = [
-    ".mp3", 
+    ".mp3",
     ".mp2",
     ".wav",
     ".m4a",
     ".MP3"]
 
-docs_exts  = [
-    ".pptx", 
-    ".pdf", 
-    ".txt", 
-    ".docx", 
-    ".xlsx", 
-    ".doc", 
-    ".csv", 
-    ".pub", 
+docs_exts = [
+    ".pptx",
+    ".pdf",
+    ".txt",
+    ".docx",
+    ".xlsx",
+    ".doc",
+    ".csv",
+    ".pub",
     ".rtf"]
 
 videos_exts = [
-    ".mp4", 
-    ".avi", 
-    ".mpg", 
+    ".mp4",
+    ".avi",
+    ".mpg",
     ".mkv",
     ".3gp",
     ".MP4",
     ".wmv"
-    ]
+]
 
 compacted_exts = [
-    ".rar", 
-    ".zip", 
+    ".rar",
+    ".zip",
     ".7z"]
 
 exec_exts = [
-    ".exe", 
-    ".EXE", 
+    ".exe",
+    ".EXE",
     ".bin"]
 
 iso_exts = [
     ".iso",
-    ]
+]
 
 
 def guess(file):
     """
-        This function gets the extension of any file
+        This function gess the extension of any file
     """
     return os.path.splitext(file)[-1]
 
-def getAll():
 
+def getAll():
     """
         This function returns a list of all extensions.
     """
     extensions = []
-    
+
     extensions.extend(image_exts)
     extensions.extend(music_exts)
     extensions.extend(docs_exts)
@@ -82,18 +82,20 @@ def getAll():
 
     return extensions
 
-def isMusic(extension):
 
+def isMusic(extension):
     res = False
     if extension in music_exts:
         res = True
     return res
 
+
 def isImage(extension):
     res = False
     if extension in image_exts:
         res = True
-    return res   
+    return res
+
 
 def isVideo(extension):
     res = False
@@ -101,11 +103,13 @@ def isVideo(extension):
         res = True
     return res
 
+
 def isDoc(extension):
     res = False
     if extension in docs_exts:
         res = True
     return res
+
 
 def isCompacted(extension):
     res = False
@@ -113,11 +117,13 @@ def isCompacted(extension):
         res = True
     return res
 
+
 def isExecutable(extension):
     res = False
     if extension in exec_exts:
         res = True
     return res
+
 
 def isIso(extension):
     res = False
