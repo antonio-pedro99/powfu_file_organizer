932409c70ca4ca233134b0699643804600684c73
Nurul-GC <nuruldecarvalho@gmail.com>
2021-05-16 02:06:54 +0200

    Update installer-win.py

--- installer-win.py
+++ installer-win.py
@@ -1,13 +1,21 @@
-import winreg, os, shutil, time
 """
     This script is to install the program in Windows machine.
 
-    Its function is to create a new Folder in C:\Program Files\ and copy the program.
+    Its function is to create a new Folder in C:\\Program Files\\ and copy the program.
     After this, create a new RegeditKey to help users to use the program by clicking on the right button.
 """
 
+import os
+import shutil
+import time
+
+# todo: python 3.7 and 3.8 doesn't have this module, please update!
+import winreg
+
 folder_name = "PowFu - File Organizer"
-installation_destination = "C:\Program Files\\"
+# don't worry, python don't need to use this slash '\'
+# because the compiler (on windows) convert this '/' to this '\'
+installation_destination = "C:/Program Files/"
 full = os.path.join(installation_destination, folder_name)
 
 
@@ -28,8 +36,8 @@ def copyFiles(file, my_path):
         if file not in os.listdir(full):
             shutil.copy(source, full)
 
-    except BaseException as e:
-        print("Something went wrong with %s"%e)
+    except BaseException as error:
+        print(f"Something went wrong with {error}")
 
 
 def createFolder():
@@ -38,28 +46,32 @@ def createFolder():
             os.makedirs(full)
         else:
             pass
-    except BaseException as e:
-        print("Something went wrong with %s"%e)
+    except BaseException as error:
+        # be careful about the variable's name
+        print(f"Something went wrong with {error}")
+
 
 def create_root_key():
     try:
-        new_key = winreg.OpenKeyEx(winreg.HKEY_CLASSES_ROOT, r"Directory\\Background\\shell", 0, winreg.KEY_ALL_ACCESS)
-        key = winreg.CreateKey(new_key,  r"powfu")
+        new_key = winreg.OpenKeyEx(winreg.HKEY_CLASSES_ROOT, "Directory/Background/shell", 0, winreg.KEY_ALL_ACCESS)
+        key = winreg.CreateKey(new_key, r"powfu")
         winreg.SetValue(key, "", winreg.REG_SZ, "Organize with PowFu - File Organizer")
         sub_key = winreg.CreateKey(key, r"command")
         winreg.SetValue(sub_key, "", winreg.REG_SZ, os.path.join(full, "powfu.exe"))
         winreg.CloseKey(new_key)
         winreg.CloseKey(key)
         winreg.CloseKey(sub_key)
-        
+
     except WindowsError as we:
-        raise "Ops! Something went wrong"
+        # to use raise we have to give a proper instance or any approximated to our thought
+        raise SystemError("Ops! Something went wrong")
+
 
 if __name__ == '__main__':
     try:
         print("=====================================================================")
-        print("======                POWFU - FILE ORGANIZER                   ======")   
-        print("======                      Installing                         ======") 
+        print("======                POWFU - FILE ORGANIZER                   ======")
+        print("======                      Installing                         ======")
         print("=====================================================================")
         time.sleep(3)
         createFolder()
